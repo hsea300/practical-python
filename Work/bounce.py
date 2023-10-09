c916fe96042c4663539302f35a2ee7de8d1b7d84
hsea300 <hugo-sea@hotmail.com>
2023-10-09 10:08:48 -0400

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+drop_height= height * 0.6
+num_bounce = 1 
+
+
+while drop_height > 1:
+  print(num_bounce, round(drop_height, ndigits=4))
+  num_bounce = num_bounce + 1
+  drop_height = drop_height * 0.6
+
+print('Number of bounces', num_bounce)
+print('Final air', round(drop_height)) 
